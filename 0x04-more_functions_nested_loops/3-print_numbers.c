51e35d50f0094c214406a38605a8e8a9ae3c5fa6
favo4r <favybliss262@gmail.com>
2024-07-28 19:01:00 +0100

    prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers 0 to 9
- *
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
 void print_numbers(void)
 {
 	char c;
